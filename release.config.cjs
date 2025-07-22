40d55a46df3451037fc966f58bd504f94da146e0
jellayy <austinlynnhuffman@gmail.com>
2025-07-21 22:49:33 -0700

    ci: fix branch ref in release config

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-    branches: ["master"],
+    branches: ["main"],
     plugins: [
         [
             '@semantic-release/commit-analyzer',
